3527656bbdd35fd425e8778a81fc8cd0ca640a79
Mariana <101410421+Placito@users.noreply.github.com>
2023-12-12 19:11:27 +0000

    icons

--- web/src/components/Icon/Icon.tsx
+++ web/src/components/Icon/Icon.tsx
@@ -1,8 +1,49 @@
 const Icon = () => {
   return (
-    <svg width="24" height="24">
-      <use href="/icons/sprite.svg#calendar" />
-    </svg>
+    <>
+      <svg width="24" height="24">
+        <use href="/icons/sprite.svg#calendar" />
+      </svg>
+      <svg width="24" height="24">
+        <use href="/icons/sprite.svg#check" />
+      </svg>
+      <svg width="24" height="24">
+        <use href="/icons/sprite.svg#chevron" />
+      </svg>
+      <svg width="24" height="24">
+        <use href="/icons/sprite.svg#close" />
+      </svg>
+      <svg width="24" height="24">
+        <use href="/icons/sprite.svg#eyeClosed" />
+      </svg>
+      <svg width="24" height="24">
+        <use href="/icons/sprite.svg#eyeOpened" />
+      </svg>
+      <svg width="24" height="24">
+        <use href="/icons/sprite.svg#logout" />
+      </svg>
+      <svg width="24" height="24">
+        <use href="/icons/sprite.svg#minus" />
+      </svg>
+      <svg width="24" height="24">
+        <use href="/icons/sprite.svg#plus" />
+      </svg>
+      <svg width="24" height="24">
+        <use href="/icons/sprite.svg#question" />
+      </svg>
+      <svg width="24" height="24">
+        <use href="/icons/sprite.svg#thumbsDown" />
+      </svg>
+      <svg width="24" height="24">
+        <use href="/icons/sprite.svg#thumbsUp" />
+      </svg>
+      <svg width="24" height="24">
+        <use href="/icons/sprite.svg#upload" />
+      </svg>
+      <svg width="24" height="24">
+        <use href="/icons/sprite.svg#user" />
+      </svg>
+    </>
   )
 }
 
