0c2a0fcfe8d6b0c1450a1ac11b0584407b94657c
Mariana <101410421+Placito@users.noreply.github.com>
2023-12-12 20:18:46 +0000

    avatar

---
+++ web/src/components/Avatar/Avatar.tsx
@@ -0,0 +1,13 @@
+const Avatar = () => {
+  return (
+    <div>
+      <img
+      src="/images/avatar-01.png"
+      alt="Amy"
+      className="h-18 w-18 rounded-full object-cover border-white border-avatar"
+      />
+    </div>
+  )
+}
+
+export default Avatar
