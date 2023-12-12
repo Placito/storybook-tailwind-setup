d515d1186b8314ee5a8ad3037e55c6913695bf2a
Mariana <101410421+Placito@users.noreply.github.com>
2023-12-12 19:35:52 +0000

    icons

--- web/src/components/Icon/Icon.stories.tsx
+++ web/src/components/Icon/Icon.stories.tsx
@@ -22,4 +22,76 @@ export default meta
 
 type Story = StoryObj<typeof Icon>
 
-export const Primary: Story = {}
+export const Calendar: Story = {
+  args: {
+    id: 'calendar',
+  },
+}
+export const Check: Story = {
+  args: {
+    id: 'check',
+  },
+}
+export const Chevron: Story = {
+  args: {
+    id: 'chevron',
+  },
+}
+export const Close: Story = {
+  args: {
+    id: 'close',
+  },
+}
+export const EyeClosed: Story = {
+  args: {
+    id: 'eyeClosed',
+  },
+}
+export const EyeOpened: Story = {
+  args: {
+    id: 'eyeOpened',
+  },
+}
+export const Logout: Story = {
+  args: {
+    id: 'logout',
+  },
+}
+export const Minus: Story = {
+  args: {
+    id: 'minus',
+  },
+}
+export const Plus: Story = {
+  args: {
+    id: 'plus',
+  },
+}
+export const Question: Story = {
+  args: {
+    id: 'question',
+  },
+}
+
+export const ThumbsDown: Story = {
+  args: {
+    id: 'thumbsDown',
+  },
+}
+
+export const ThumbsUp: Story = {
+  args: {
+    id: 'thumbsUp',
+  },
+}
+export const Upload: Story = {
+  args: {
+    id: 'upload',
+  },
+}
+
+export const User: Story = {
+  args: {
+    id: 'user',
+  },
+}
