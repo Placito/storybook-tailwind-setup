9986321dacd58b36b7e7e62288f6372152c753b2
Mariana <101410421+Placito@users.noreply.github.com>
2023-12-14 10:38:08 +0000

    avatar.tsx file

--- web/src/components/Avatar/Avatar.tsx
+++ web/src/components/Avatar/Avatar.tsx
@@ -1,11 +1,19 @@
+import Icon from "../Icon/Icon"
+
 const Avatar = () => {
   return (
     <div>
       <img
-      src="/images/avatar-01.png"
-      alt="Amy"
-      className="h-18 w-18 rounded-full object-cover border-white border-avatar"
+        src="/images/avatar-01.png"
+        alt="Amy"
+        className="h-18 w-18 rounded-full border-avatar border-white object-cover"
       />
+      <div className="center h-18 w-18 rounded-full border-avatar border-white bg-spanishGreen text-2xl font-bold uppercase text-white">
+        A
+      </div>
+      <div className="center h-18 w-18 rounded-full border-avatar border-white bg-spanishGreen text-2xl font-bold uppercase text-white">
+        <Icon id="eyeClosed" size={32} />
+      </div>
     </div>
   )
 }
