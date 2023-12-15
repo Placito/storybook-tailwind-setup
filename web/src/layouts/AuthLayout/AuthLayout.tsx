aae18f3d99c7fc4eda2bc1817b4f7ff41ce6764b
Mariana <101410421+Placito@users.noreply.github.com>
2023-12-15 19:35:53 +0000

    chalenge 6

--- web/src/layouts/AuthLayout/AuthLayout.tsx
+++ web/src/layouts/AuthLayout/AuthLayout.tsx
@@ -1,17 +1,22 @@
+import Footer from 'src/components/Footer/Footer'
+
 type AuthLayoutProps = {
   children?: React.ReactNode
 }
 
 const AuthLayout = ({ children }: AuthLayoutProps) => {
   return (
-    <main className="min-w-screen min-[1440px]:bg-[lenght:100%_auto] bg[center_top] min-h-screen bg-silverTree bg-auth bg-[length:1440px_auto] bg-no-repeat dark:bg-nileBlue">
-      <img
-        src="/images/logo__secret-santa.svg"
-        alt="Secret Santa"
-        className="mx-auto mb-24 w-[460px] pt-24"
-      />
-      {children}
-    </main>
+    <>
+      <main className="min-w-screen min-[1440px]:bg-[lenght:100%_auto] bg[center_top] min-h-screen bg-silverTree bg-auth bg-[length:1440px_auto] bg-no-repeat dark:bg-nileBlue">
+        <img
+          src="/images/logo__secret-santa.svg"
+          alt="Secret Santa"
+          className="mx-auto mb-24 w-[460px] pt-24"
+        />
+        {children}
+      </main>
+      <Footer />
+    </>
   )
 }
 
